eb7a7ee593700bbf74ae71af1d46995b6593dd0c
georgelucasvieira <george.vieira.carmo@gmail.com>
2026-05-17 22:53:38 -0300

    estrutura da hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
+      <head> 
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet"/>
       </head>
       <body className="min-h-full min-w-full">{children}</body>
     </html>
--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -2,18 +2,19 @@ import SlideShow from "./SlideShow";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col w-full h-screen justify-center text-center">
+    <section className="flex flex-col w-full h-screen justify-center text-center items-center">
       <SlideShow />
       <Content />
+      <Countdown />
     </section>
   );
 }
 
 function Content() {
   return (
-    <div className="text-white w-full justify-center items-center">
-      <h1 className="font-freight">
-        <span>Aurora</span><span> &amp; </span><span>George</span>
+    <div className="relative text-white w-full justify-center items-center">
+      <h1 className="font-cormorant after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:h-px after:w-1/12 after:bg-white">
+        <span>AURORA</span><span> &amp; </span><span>GEORGE</span>
       </h1>
       <h2>nas ditas linhas em que nos encontramos</h2>
       <span>08 DE AGOSTO DE 2026</span>
@@ -21,3 +22,48 @@ function Content() {
     </div>
   );
 }
+
+function Countdown() {
+  return (
+    <section className="font-cormorant text-white text-5xl relative after:absolute after:bg-transparent after:bg-[url(/images/ripped-bg-3.png)] after:bg-no-repeat after:bg-bottom after:bg-contain after:w-full after:h-full">
+      <div >  
+        <div >
+          <div >Casados há:</div>
+          <div className="flex gap-3">
+            <div>
+              <div >10</div>
+              <div >Meses</div>
+            </div>
+            <div>
+              <div >22</div>
+              <div >Dias</div>
+            </div>
+            <div>
+              <div >22</div>
+              <div >Horas</div>
+            </div>
+            <div>
+              <div >27</div>
+              <div >Minutos</div>
+            </div>
+            <div >
+              <div >44</div>
+              <div >Segundos</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+
+}
+
+// background: url(/countdown/ripped-bg-3.png);
+// background-color: transparent;
+// background-size: contain;
+// background-repeat: no-repeat;
+// background-position: bottom;
+// position: absolute;
+// content: "";
+// width: 100%;
+// height: 100%;
