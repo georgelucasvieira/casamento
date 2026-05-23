b8032e7e08ec3f66d6aea0102330124d17f36a71
georgelucasvieira <george.vieira.carmo@gmail.com>
2026-05-23 19:00:27 -0300

    pagina de instrucoes

--- app/instrucoes/page.tsx
+++ app/instrucoes/page.tsx
@@ -4,12 +4,38 @@ export default async function InstrucoesPage() {
   return (
     <main className="relative min-h-screen">
       <Navbar />
-      <section className="px-6 py-24 lg:px-12">
-        <div className="mx-auto max-w-5xl">
-          <div className="rounded-[2rem] bg-white p-8 shadow">
-            <h1 className="text-2xl font-semibold mb-6">Instruções</h1>
+      <section className="mt-40 px-6 lg:px-12 font-cormorant text-2xl text-center">
+        <div className="w-full mx-auto max-w-250 flex flex-col gap-16 items-center justify-center">
+          <div className='w-full flex flex-col items-center justify-center'>
+            <div>Queridos convidados,</div>
+            <div>
+              Nossa cerimônia será celebrada em rito religioso, durante a Santa Missa.
+              Por isso, pedimos a gentileza de observarem o respeito devido ao ambiente sagrado
+              e ao sacramento que estaremos celebrando.
+            </div>
+          </div>
+
+          <div className='w-full flex flex-col items-center justify-center'>
+            <div>Gentilmente pedimos cuidado ao traje escolhido:</div>
+            <div>
+              Às mulheres, evitem vestidos muito justos, transparências, decotes acentuados ou fendas muito profundas.
+              Aos homens, sugerimos trajes compatíveis com a solenidade da ocasião, evitando bermudas, regatas e peças excessivamente informais.
+            </div>
+          </div>
+          
+          <div className='w-full text-center'>
+            <span className='text-3xl font-cormorant-700'>Traje sugerido: Esporte fino</span>
           </div>
         </div>
       </section>
-    </main>  );
+      <section className="mt-50 px-6 lg:px-12 flex flex-col gap-6 items-center justify-center font-cormorant text-2xl text-center">
+        <div className='w-full text-center text-'>
+          Abaixo, inspirações para este dia especial:
+        </div>
+        <div className='w-full flex flex-col items-center justify-center lg:flex-row'>
+          <img src="/images/dress-code.png" alt="Dress Code" className='md:w-[62%]'/>
+        </div>
+      </section>
+      <div className='h-25'/>
+    </main>);
 }
