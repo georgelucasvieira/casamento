64ba01ea324ef4474b01b0366ae3155d1c86571e
georgelucasvieira <george.vieira.carmo@gmail.com>
2026-05-23 19:29:47 -0300

    ajustes localizacao

--- components/Localizacao.tsx
+++ components/Localizacao.tsx
@@ -4,46 +4,50 @@ export const Localizacao = () => {
     return (
         <>
             <section className="px-6 py-24 lg:px-12 font-cormorant">
-                <div className="mx-auto max-w-5xl">
+                <div className="mx-auto max-w-7xl">
                     <div className="rounded-[2.5rem] px-8 py-12">
-                        <div className="grid gap-12 lg:grid-cols-[1.4fr_0.8fr]">
-                            <div>
-                                <p className="text-sm uppercase tracking-[0.35em] text-black/50">
-                                    Localização
-                                </p>
-                                <h1 className="mt-6 text-5xl font-semibold text-black">
-                                    Onde será o casamento
-                                </h1>
-                                <p className="mt-6 max-w-2xl text-lg text-black/70">
-                                    Nosso casamento será celebrado na Paróquia Santa Genoveva em Goiânia com Missa.
-                                </p>
+
+                        <div className='flex gap-16 justify-center'>
+                            <div className='h-full'>
+                                <img src="/images/paroquia-aquarela.jpg" alt="Paróquia Santa Genoveva" className='w-full h-full object-contain rounded-4xl' />
                             </div>
 
-                            <div className="space-y-6 rounded-[2rem] bg-white p-8 shadow-[0_15px_40px_rgba(0,0,0,0.08)]">
+                            <div className="flex flex-col gap-12 lg:min-w-100">
                                 <div>
-                                    <p className="text-sm uppercase tracking-[0.25em] text-black/40">
-                                        Endereço
+                                    <p className="text-sm uppercase tracking-[0.35em] text-black/50">
+                                        Localização
                                     </p>
-                                    <h2 className="mt-4 text-3xl font-semibold text-black">
-                                        Av. Brasil, Santa Genoveva
-                                    </h2>
-                                    <p className="mt-3 text-lg text-black/70">
-                                        Goiânia, Goiás 74670-010 • 18:00
+                                    <span className="mt-6 text-6xl font-semibold text-black">
+                                        Onde será o casamento
+                                    </span>
+                                    <p className="mt-6 max-w-2xl text-lg text-black/70">
+                                        Nosso casamento será celebrado na Paróquia Santa Genoveva em Goiânia com Missa.
                                     </p>
                                 </div>
-                                <div>
-                                    <p className="text-sm uppercase tracking-[0.25em] text-black/40">
-                                        Observação
-                                    </p>
-                                    <p className="mt-3 text-lg text-black/70">
-                                        A celebração será realizada pontualmente com o Padre, favor chegar com antecedência.
-                                    </p>
+
+                                <div className="rounded-4xl bg-white p-8 shadow-[0_15px_40px_rgba(0,0,0,0.08)]">
+                                    <div>
+                                        <p className="text-sm uppercase tracking-[0.25em] text-black/40">
+                                            Endereço
+                                        </p>
+                                        <h2 className="mt-4 text-3xl font-semibold text-black">
+                                            Av. Brasil, Santa Genoveva
+                                        </h2>
+                                        <p className="mt-3 text-lg text-black/70">
+                                            Goiânia, Goiás 74670-010 • 18:00
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="text-sm uppercase tracking-[0.25em] text-black/40">
+                                            Observação
+                                        </p>
+                                        <p className="mt-3 text-lg text-black/70">
+                                            A celebração será realizada pontualmente com o Padre, favor chegar com antecedência.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className='w-full h-200 mt-16'>
-                            <img src="/images/paroquia-aquarela.jpg" alt="Paróquia Santa Genoveva" className='w-full h-full object-contain'/>
                         </div>
 
                         <div className="mt-12 overflow-hidden rounded-[2rem] border border-black/10 shadow-[0_20px_70px_rgba(0,0,0,0.08)]">
